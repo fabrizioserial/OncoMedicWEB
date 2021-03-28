efb89e7a4351999a0b792c713ee2f8b32c3b3945
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2021-03-28 13:43:51 -0300

    flexed buttons

--- src/component/home/buttonsHome/ButtonHome.js
+++ src/component/home/buttonsHome/ButtonHome.js
@@ -5,7 +5,7 @@ import { Router,Link, Route, Switch } from 'react-router-dom'
 
 export const ButtonHome = (props) => {
     return(
-            <Link to={props.link} className="">
+            <Link to={props.link}>
             <button className={props.color} onClick={props.onClick}>
                 <p>{props.text}</p>
             </button></Link> 
--- src/component/home/modals/ModalPopOverVerRegistroDiario.js
+++ src/component/home/modals/ModalPopOverVerRegistroDiario.js
@@ -4,6 +4,8 @@ import 'fontsource-roboto';
 import {ItemUser} from '../../ItemUser/ItemUser'
 
 
+
+
 const ModalPopOverVerRegistroDiario = (props) => {
       const i = [1,2,3,4,5,6]
      
