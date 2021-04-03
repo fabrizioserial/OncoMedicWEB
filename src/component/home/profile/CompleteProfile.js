8d66a006afc380052445b45f07c26df3af89ec64
FABRI Ricardo <fabrizio.serial@ing.austral.edu.ar>
2021-04-02 23:22:19 -0300

    mm

--- src/component/home/profile/CompleteProfile.js
+++ src/component/home/profile/CompleteProfile.js
@@ -1,28 +1,22 @@
 import React from 'react'
 import "../profile/CompleteProfile.css"
 import {ButtonGoBack} from "../seeAllUsers/ButtonGoBack"
-import { UsertabEstado } from '../usertabEstado/UsertabEstado'
+import { UsertabEstado } from '../usertabEstado/UsertabEstado.js'
 import { UsertabSintomas } from '../usertabSintomas/UsertabSintomas'
+import ProfileTab from './profileTab/ProfileTab'
 
 export const CompleteProfile = () => {
     return (
         <div className="profile-cont-background">
             <ButtonGoBack text="VOLVER AL INICIO" color="purple"></ButtonGoBack>
-            <div className="name-div-complete-profile">
-                <img className="complete-profile-user-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMRyv9Dkf8Wusb0uForhlXoz090E0Xgt_1OQ&usqp=CAU" />
-                <div className="div-text-complete-profile">
-                    <p className="id-complete-profile">42673854</p>
-                    <h3 className="name-complete-profile">Altamirano, Carmen</h3>
-                    <p className="id-complete-profile">Tipo de cancer: </p>
-                    <p className="id-complete-profile">Fuma: </p>
-                    <p className="id-complete-profile">Diabetes: </p>
-                </div>
-            </div>
+            <ProfileTab/>
             <div className="two-squares-complete-profile">
-                <UsertabEstado flexi={{Flex:1}}/>
-                
-                <UsertabSintomas flexi={{Flex:1}}/>
-                
+                <div  className="estado-usertab-cont-background">
+                    <UsertabEstado type="profile" flexi={{Flex:1}}/>
+                </div>
+                <div className="sintoms-usertab-cont-background">
+                    <UsertabSintomas flexi={{Flex:1}}/>
+                </div>  
             </div>
         </div>
     )
