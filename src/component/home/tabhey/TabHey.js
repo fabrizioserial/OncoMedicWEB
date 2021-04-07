fe67d2c7bb705c017284f8d98038126645718a4c
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2021-04-07 11:05:54 -0300

    notifications

--- src/component/home/tabhey/TabHey.js
+++ src/component/home/tabhey/TabHey.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import '../tabhey/TabHey.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCog,faBell } from '@fortawesome/free-solid-svg-icons'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
 import { Tuerquita } from '../../tuerquita/Tuerquita.js'
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { CustomMenuItem } from '../../customMenuItem/CustomMenuItem'
 
 
 
 export const TabHey = (props) => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
     return(
             <div className="tabhey-cont-background">
                 <div className="tabhey-cont">
@@ -19,9 +37,33 @@ export const TabHey = (props) => {
                        <Tuerquita style="home"/>
                     </div>
                     
-                    <button className="tabhey-btn-options">
+                    <button aria-describedby={id} className="tabhey-btn-options" onClick={handleClick}>
                         <FontAwesomeIcon icon={faBell}/>
                     </button>
+
+                        
+                        <Menu className="menu-1"
+                            id={id}
+                            open={open}
+                            anchorEl={anchorEl}
+                            onClose={handleClose}
+                            anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
+                            }}
+                            transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                            }}
+                            PaperProps={{
+                            style: { width: '300px',marginTop: '2px' },
+                            }}
+                        >
+                            <CustomMenuItem name="Serial, Fabrizio" id="666" onClick={handleClose}/>
+
+
+
+                        </Menu>
                 </div>
             </div>
     )
