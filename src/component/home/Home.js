9743f7dd75169dd1d80c9bcb34c70758520437f3
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2021-04-28 11:02:27 -0300

    prueba

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -134,7 +134,7 @@ const Home = ({medicData}) =>{
     }
 
     useEffect(()=>{
-        console.log("se actualizo",symptomsList2)
+        console.log("se actualizoo",symptomsList2)
     },[symptomsList2])
 
     useEffect(()=>{
