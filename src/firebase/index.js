aeb1c08dcd4c6bba8093016ccaaecbf1f967a599
FABRI Ricardo <fabrizio.serial@ing.austral.edu.ar>
2021-05-12 11:35:32 -0300

    env implemented and working

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,11 @@
 import firebase from 'firebase/app';
 import '@firebase/firestore';
 
-const app = firebase.initializeApp({
-    apiKey: "AIzaSyDcinCzje9E9uwyxVhe3So1-Aa4jWmrV8s",
-    authDomain: "oncologia-austral.firebaseapp.com",
-    databaseURL: "https://oncologia-austral-default-rtdb.firebaseio.com",
-    projectId: "oncologia-austral",
-    storageBucket: "oncologia-austral.appspot.com",
-    messagingSenderId: "497357435658",
-    appId: "1:497357435658:web:e04ca1161344c718fc40f3"
-})
+const APIKEY = process.env.REACT_APP_FIREBASECONFIG
+
+console.log(APIKEY)
+
+const app = firebase.initializeApp(APIKEY)
 
 export function getFirebase() {
     return app;
