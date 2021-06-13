4b987ae01200ee04109a632fbe69a45138f0f8c0
FABRI Ricardo <fabrizio.serial@ing.austral.edu.ar>
2021-06-13 14:08:16 -0300

    heroku fix 7

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,6 +4,7 @@ import '@firebase/firestore';
 
 const app = firebase.initializeApp(JSON.parse(process.env.REACT_APP_FIREBASECONFIG))
 console.log(process.env.REACT_APP_FIREBASECONFIG)
+console.log(JSON.parse(process.env.REACT_APP_FIREBASECONFIG))
 
 export function getFirebase() {
     return app;
